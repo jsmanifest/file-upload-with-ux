684f9f49148e8dfb23f4a958b902d0c91d7d2028
pfftdammitchris <pfftdammitchris@gmail.com>
2019-06-16 08:40:09 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
 import React from 'react'
-import useImgLoader from './useImgLoader'
+import useFileHandlers from './useFileHandlers'
 import './App.css'
 
 const Input = (props) => (
   <input
-    type='file'
-    accept='image/*'
-    name='img-loader-input'
+    type="file"
+    accept="image/*"
+    name="img-loader-input"
     multiple
     {...props}
   />
 )
 
 const App = () => {
-  const { files, onSubmit, onChange } = useImgLoader()
+  const {
+    files,
+    pending,
+    next,
+    uploading,
+    uploaded,
+    status,
+    onSubmit,
+    onChange,
+  } = useFileHandlers()
 
   return (
-    <div className='container'>
-      <form className='form' onSubmit={onSubmit}>
+    <div className="container">
+      <form className="form" onSubmit={onSubmit}>
+        {status === 'FILES_UPLOADED' && (
+          <div className="success-container">
+            <div>
+              <h2>Congratulations!</h2>
+              <small>You uploaded your files. Get some rest.</small>
+            </div>
+          </div>
+        )}
         <div>
           <Input onChange={onChange} />
-          <button type='submit' className='submit-btn'>
+          <button type="submit" className="submit-btn">
             Submit
           </button>
         </div>
         <div>
-          {files.map(({ name, src }, index) => (
-            <div key={`thumb${index}`} className='thumbnail-wrapper'>
-              <img className='thumbnail' src={src} alt='' />
-              <div className='thumbnail-caption'>{name}</div>
+          {files.map(({ file, src, id }, index) => (
+            <div
+              style={{
+                opacity: uploaded[id] ? 0.2 : 1,
+              }}
+              key={`thumb${index}`}
+              className="thumbnail-wrapper"
+            >
+              <img className="thumbnail" src={src} alt="" />
+              <div className="thumbnail-caption">{file.name}</div>
             </div>
           ))}
         </div>
